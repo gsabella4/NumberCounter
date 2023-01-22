13ddde1726bf953422389e313dae2a1db95fadda
Greg Sabella <106336554+gsabella4@users.noreply.github.com>
2023-01-22 17:12:01 -0500

    added some features like random, add5, take5, some styling.

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const countLabel = document.getElementById("countLabel");
 const decreaseBtn = document.getElementById("decreaseBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 const resetBtn = document.getElementById("resetBtn");
+const add5Btn = document.getElementById("plus5");
+const take5Btn = document.getElementById("minus5");
+const randomBtn = document.getElementById("randomNum");
 
-
-
+function randomNum(){
+      let random = Math.floor(Math.random()*1001);
+      count = random;
+      countLabel.innerText = count;
+}
 
 function decreaseCount(){
       count-=1;
@@ -15,6 +21,16 @@ function decreaseCount(){
 
 function increaseCount(){
       count+=1;
+      countLabel.innerText = count;
+}
+
+function add5(){
+      count+=5;
+      countLabel.innerText = count;
+}
+
+function take5(){
+      count-=5;
       countLabel.innerText = count;
 }
 
@@ -40,6 +56,18 @@ increaseBtn.addEventListener('click', () =>{
 resetBtn.addEventListener('click', () =>{
       resetCount();
 });
+
+add5Btn.addEventListener('click', () =>{
+      add5();
+});
+
+take5Btn.addEventListener('click', () =>{
+      take5();
+});
+
+randomBtn.addEventListener('click', () => {
+      randomNum();
+})
 }
 
 
