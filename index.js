cfeededb7c8224184a2334601dc432287f7c20e9
Greg Sabella <106336554+gsabella4@users.noreply.github.com>
2023-01-22 22:20:53 -0500

    pushing tweaks to design

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const countLabel = document.getElementById("countLabel");
 const decreaseBtn = document.getElementById("decreaseBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 const resetBtn = document.getElementById("resetBtn");
-const add5Btn = document.getElementById("plus5");
-const take5Btn = document.getElementById("minus5");
+const add5Btn = document.getElementById("add5");
+const take5Btn = document.getElementById("take5");
 const randomBtn = document.getElementById("randomNum");
 
-function randomNum(){
-      let random = Math.floor(Math.random()*1001);
-      count = random;
-      countLabel.innerText = count;
-}
 
-function decreaseCount(){
-      count-=1;
+function add5(){
+      count+=5;
       countLabel.innerText = count;
 }
 
@@ -24,8 +19,8 @@ function increaseCount(){
       countLabel.innerText = count;
 }
 
-function add5(){
-      count+=5;
+function decreaseCount(){
+      count-=1;
       countLabel.innerText = count;
 }
 
@@ -34,40 +29,45 @@ function take5(){
       countLabel.innerText = count;
 }
 
+function randomNum(){
+      let random = Math.floor(Math.random()*5001);
+      count = random;
+      countLabel.innerText = count;
+}
+
 function resetCount(){
-      count = 0;
+      count=0;
       countLabel.innerText = count;
       window.alert("Count has been reset!");
 }
 
-
-
 document.addEventListener("DOMContenLoaded", begin());
 
 function begin(){
-decreaseBtn.addEventListener('click', () =>{
-      decreaseCount();
+
+add5Btn.addEventListener('click', () =>{
+      add5();
 });
 
 increaseBtn.addEventListener('click', () =>{
       increaseCount();
 });
 
-resetBtn.addEventListener('click', () =>{
-      resetCount();
-});
-
-add5Btn.addEventListener('click', () =>{
-      add5();
+decreaseBtn.addEventListener('click', () =>{
+      decreaseCount();
 });
 
 take5Btn.addEventListener('click', () =>{
       take5();
 });
 
-randomBtn.addEventListener('click', () => {
+randomBtn.addEventListener('click', () =>{
       randomNum();
-})
+});
+
+resetBtn.addEventListener('click', () =>{
+      resetCount();
+});
 }
 
 
